58227ee815d6f311511182f45cea3a1ee35b0387
mbehi <mohsin.behi@gmail.com>
2021-05-21 09:37:22 -0500

    complete code challenge 09

--- code-challenges/challenges-09.test.js
+++ code-challenges/challenges-09.test.js
@@ -10,6 +10,8 @@ E.g. [4,2,7,5,9,2] -> 9
 ------------------------------------------------------------------------------------------------ */
 const maxInArray = (arr) => {
   // Solution code here...
+  let maxInArray = arr.reduce((acc,curr) => Math.max(acc,curr), arr[0]);
+  return maxInArray;
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -26,6 +28,8 @@ const courseInfo = { name: 'Code 301', duration: { dayTrack: '4 weeks', eveningT
 
 const getCourseKeys = (obj) => {
   // Solution code here...
+  let getCourseKeys = Object.entries(obj).map(item => item[0]);
+  return getCourseKeys;
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -38,6 +42,8 @@ Write a function named checkValues that takes in an object and a value and retur
 
 const checkValues = (obj, value) => {
   // Solution code here...
+  let checkValues = Object.values(obj).includes(value);
+  return checkValues;
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -61,6 +67,8 @@ HR has asked you to change the data to make it easier to print so that it looks 
 
 const updateNumbers = (obj) => {
   // Solution code here...
+  let updateNumbers = Object.entries(obj).map(nums => nums.join(': '));
+  return updateNumbers;
 };
 
 
@@ -117,6 +125,9 @@ const characters = [
 const getHouses = (arr) => {
   let houses = [];
   // Solution code here...
+  Object.values(arr).forEach(characters => {
+    houses.push(characters.house);
+  });
   return houses;
 };
 
@@ -134,7 +145,12 @@ hasChildrenValues(characters, 'Sansa') will return false
 
 const hasChildrenValues = (arr, character) => {
   // Solution code here...
-
+  for(let obj of arr){
+    if(Object.values(obj).includes(character)) {
+      return obj.children && obj.children.length;
+    }
+  }
+  return false;
 };
 
 /* ------------------------------------------------------------------------------------------------
